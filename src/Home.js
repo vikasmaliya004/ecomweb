b4b8a6903543b1ecba625d79f56a44221c288b6b
Vikas Maliya <maliyavikas7@gmail.com>
2022-09-02 10:02:11 +0000

    done vikasa

--- src/Home.js
+++ src/Home.js
@@ -1,24 +1,24 @@
 import React from "react";
-import Navbar from "./navbar";
+// import Navbar from "./navbar";
 import Products from "./Products";
 
 const Home = () => {
   return (
     <div className="hero">
-      <div class="card bg-dark text-white border-0">
+      <div className="card bg-dark text-white border-0">
         <img
           src="https://www.saturdayeveningpost.com/wp-content/uploads/satevepost/2020-08-17-woman-shopping-shutterstock-860x573.jpg"
           class="card-img"
           alt="Background"
           height="500px"
         />
-        <div class="card-img-overlay d-flex flex-column justify-content-center">
+        <div className="card-img-overlay d-flex flex-column justify-content-center">
           <div className="container"></div>
-          <h5 class="card-title display-3 fw-bolder mb-0">
+          <h5 className="card-title display-3 fw-bolder mb-0">
             NEW SEASON ARRIVALS
           </h5>
-          <p class="card-text lead fs-1 " >CHICK OU T ALL TRENDS</p>
-          <p class="card-text">Last updated 3 mins ago</p>
+          <p className="card-text lead fs-1 " >CHICK OU T ALL TRENDS</p>
+          <p className="card-text">Last updated 3 mins ago</p>
         </div>
       </div>
       <Products/>
--- src/Products.js
+++ src/Products.js
@@ -5,16 +5,16 @@ const Products = () => {
   const [data, setData] = useState([]);
   const [filter, setfilter] = useState(data);
   const [loading, setLoading] = useState(false);
-  let componentMounted = true;
   useEffect(() => {
+  let componentMounted = true;
     const getProducts = async () => {
       setLoading(true);
       const response = await fetch("https://fakestoreapi.com/products");
-      if (componentMounted) {
+      if ( componentMounted) {
         setData(await response.clone().json());
         setfilter(await response.json());
         setLoading(false);
-        console.log(filter);
+        // console.log(filter);
       }
 
       return () => {
