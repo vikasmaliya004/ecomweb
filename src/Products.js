648a915d50ebe4fb491b985d74ce8f352b1b4b11
vikasmaliya004 <vikasmaliya004@gmail.com>
2022-09-16 23:32:23 +0530

    project done

--- src/Products.js
+++ src/Products.js
@@ -1,30 +1,30 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import Skeleton from "react-loading-skeleton";
 import { NavLink } from "react-router-dom";
 const Products = () => {
   const [data, setData] = useState([]);
   const [filter, setfilter] = useState(data);
   const [loading, setLoading] = useState(false);
-  let componentMounted = true;
+  let componentMounted = useRef(true);
   useEffect(() => {
     const getProducts = async () => {
       setLoading(true);
       const response = await fetch("https://fakestoreapi.com/products");
-      if (componentMounted) {
+      if (componentMounted.current) {
         setData(await response.clone().json());
         setfilter(await response.json());
         setLoading(false);
-        console.log(filter);
+        // console.log(filter);
       }
 
       return () => {
-        componentMounted = false;
+        componentMounted.current = false;
       };
     };
     getProducts();
   }, []);
   const Loading = () => {
-    return <>
+    return ( <>
     <div className="col-md-3">
         <Skeleton height = {350}/>
     </div>
@@ -38,6 +38,7 @@ const Products = () => {
         <Skeleton height = {350}/>
     </div>
     </>
+    );
   };
   const filterProduct = (cat) =>{
     const updatedList = data.filter((x)=>x.category===cat);
